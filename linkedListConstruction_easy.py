8f587691a7e01e2f75e3e5422575dfb064291ede
Ricky <rickdhanota@gmail.com>
2020-10-27 19:07:17 -0700

    initial commit

---
+++ linkedListConstruction_easy.py
@@ -0,0 +1,9 @@
+# Linked List construction
+
+#Write a DoublyLinkedList class that has a head and a tail, both of which point to either a linked list Node or None/null. The class should support:
+#   -Setting the head and tail of the linked list
+#   -Inserting the nodes before and after the other nodes as well as at given positions (the position of the head node is 1)
+#   -Removing given nodes and removing nodes with given values
+#   -Searching for nodes with given values
+
+#
